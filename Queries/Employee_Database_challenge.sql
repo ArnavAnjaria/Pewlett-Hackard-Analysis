b2695344e83c5a3bf83733796ab1c978860e86b0
ArnavAnjaria <anjaria.arnav@gmail.com>
2022-01-08 13:59:21 -0800

    Deliverable 1 Complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -9,6 +9,19 @@ FROM employees as e
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-order by e.emp_no;
+ORDER BY e.emp_no;
 
 SELECT * from retirement_titles;
+
+-- remove duplicates
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name
+title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT * from unique_titles;
